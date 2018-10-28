e17b52b6f31dcdb224b17c52563131a17f1e84ab
Philip Alb <philip.alb@gmx.de>
2018-10-28 15:36:56 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/sfgrecipesapp/SfgRecipesAppApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgrecipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipesAppApplication.class, args);
+	}
+}
