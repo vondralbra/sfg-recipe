79be4294eefc0158563441e668a0fb7fad8e2821
Philip Alb <philip.alb@gmx.de>
2018-11-03 16:16:16 +0100

    Assigment

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,7 +1,14 @@
 package guru.springframework.domain;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /**
  * Created by jt on 6/13/17.
@@ -9,46 +16,55 @@ import java.math.BigDecimal;
 @Entity
 public class Ingredient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String description;
-    private BigDecimal amount;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
 
-    @ManyToOne
-    private Recipe recipe;
+	@ManyToOne
+	private Recipe recipe;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public BigDecimal getAmount() {
-        return amount;
-    }
+	public BigDecimal getAmount() {
+		return amount;
+	}
 
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
+	public Recipe getRecipe() {
+		return recipe;
+	}
 
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUnitOfMeasure() {
+		return uom;
+	}
+
+	public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+		this.uom = unitOfMeasure;
+	}
 }
